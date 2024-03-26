dbf58dcf3a53ef671fb2ec620a92afe2fb24516e
serchg0george <serchg0george@gmail.com>
2024-03-26 16:37:58 +0200

    Deleted unused import

--- src/main/java/com/bookstore/dto/OrderItemDto.java
+++ src/main/java/com/bookstore/dto/OrderItemDto.java
@@ -3,7 +3,6 @@ package com.bookstore.dto;
 import com.bookstore.dto.base.BaseDto;
 import com.bookstore.entity.BookEntity;
 import com.bookstore.entity.OrderEntity;
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
