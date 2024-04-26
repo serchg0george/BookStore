fe432b0cc241c5c9ca1180527ee8317ced9ddd2b
serchg0george <serchg0george@gmail.com>
2024-04-26 14:44:38 +0300

    Fixed implementation to work with swagger ui

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -12,8 +12,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .authorizeHttpRequests(request -> request.requestMatchers(AUTH_WHITELIST).permitAll())
                 .authorizeHttpRequests(request -> request.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()));
         return http.build();
     }
+
+    private static final String[] AUTH_WHITELIST = {
+            "/api/v1/auth/**",
+            "/v2/api-docs",
+            "/v3/api-docs",
+            "/v3/api-docs/**",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/swagger-ui.html"
+    };
 }
